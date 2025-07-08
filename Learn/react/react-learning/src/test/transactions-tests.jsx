3c19a4ec60971a487b3df045cb0162ec33931515
atsotsomanski <sn3ts@outlook.com>
2025-07-09 00:41:00 +0300

    removed redundant code

---
+++ Learn/react/react-learning/src/test/transactions-tests.jsx
@@ -0,0 +1,13 @@
+const TransactionsTests = () => {
+  const adresses = {
+    APP_ETHERFI: '0x0ba76d617b6de9643f30dd896192b1ca5ef6ad7c',
+    BINANCE: '0xb71dfc4c1f273cf97f1bac46f2e3cbaf3b12b2e0',
+    METAMASK: '0x0ba76d617B6DE9643F30dD896192B1ca5EF6Ad7c',
+    COINBASE: '0x4365C149b6FDd95EBf074727Df603a635782C4D4',
+    LEDGER: '0xf82433E3980BdB445625Be4b519369D1162C797F'
+  };
+
+  
+  
+  return <div></div>
+}
